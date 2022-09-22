4372d61b65dd73a9440112271be32c333ded59ad
j-guigon <87800430+j-guigon@users.noreply.github.com>
2022-09-22 15:58:18 -0700

    Added method that authenticates the length of an input string and returns a boolean value. It also passed two JUnit tests.

--- src/functions.java
+++ src/functions.java
@@ -1,2 +1,9 @@
 public class functions {
+    //password, min length, reccommended length, maximum length
+    public static boolean authenticateLength(String password, int min){
+        if(min <= password.length())
+            return true;
+        else
+            return false;
+    }
 }
--- tests/function_tests.java
+++ tests/function_tests.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class function_tests {
+
+    @Test
+    public void testAuthenticateLengthTrue(){
+        assertEquals(true, functions.authenticateLength("Christmas", 4));
+    }
+    @Test
+    public void testAuthenticateLengthFalse(){
+        assertEquals(false, functions.authenticateLength("Christmas", 10));
+    }
 }
